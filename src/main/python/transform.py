c4e680e7c3aff3ced83d6cb1ed833e15ad5a0f3b
Raphael Ehret <ehretrapha@eisti.eu>
2017-04-04 18:15:37 +0200

    rm : deleted renamed files

--- src/main/python/transform.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext
-
-def transform(imageList, transformation):
-    sc = SparkContext("local", "Parallelized Transformation")
-    rdd = sc.parallelize(imageList)
-    transformedImages = rdd.map(transformation).collect()
-    return transformedImages
